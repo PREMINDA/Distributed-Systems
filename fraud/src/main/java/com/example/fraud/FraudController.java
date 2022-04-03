e43ea8a5d12b715a1d365e22b3f8a78aaf677f9b
PREMINDA <premindabandara0777@gmail.com>
2022-04-03 04:35:44 -0700

    log info in controller

--- fraud/src/main/java/com/example/fraud/FraudController.java
+++ fraud/src/main/java/com/example/fraud/FraudController.java
@@ -1,13 +1,16 @@
 package com.example.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 public record FraudController(FraudCheckService fraudCheckService) {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
        boolean isFraudCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
        return new FraudCheckResponse(isFraudCustomer);
     }
 }
