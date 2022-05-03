3ce7355c2cc7546b377388d921c6240d26005457
PREMINDA <premindabandara0777@gmail.com>
2022-05-04 04:27:40 +0530

    Add EurekaServer

--- fraud/src/main/java/com/example/fraud/FraudController.java
+++ fraud/src/main/java/com/example/fraud/FraudController.java
@@ -1,14 +1,21 @@
 package com.example.fraud;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
-public record FraudController(FraudCheckService fraudCheckService) {
+@AllArgsConstructor
+public class FraudController {
+
+    private final FraudCheckService fraudCheckService;
+
     @GetMapping(path = "{customerId}")
-    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
+    public FraudCheckResponse isFraudster(
+            @PathVariable("customerId") Integer customerId
+    ){
        boolean isFraudCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         log.info("fraud check request for customer {}", customerId);
        return new FraudCheckResponse(isFraudCustomer);
