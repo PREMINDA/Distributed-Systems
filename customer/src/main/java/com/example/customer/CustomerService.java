333fe698f1091a8aea8d61eeddaa78b9a4d9e621
PREMINDA <premindabandara0777@gmail.com>
2022-05-04 04:06:22 +0530

    Check Fraudster

--- customer/src/main/java/com/example/customer/CustomerService.java
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -1,16 +1,31 @@
 package com.example.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService{
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest){
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
                 .lastName(customerRegistrationRequest.lastName())
                 .email(customerRegistrationRequest.email())
                 .build();
+        customerRepository.saveAndFlush(customer);
 
-        customerRepository.save(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster");
+        }
     }
 }
---
+++ customer/src/main/java/com/example/customer/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.example.customer;
+
+public record FraudCheckResponse(boolean isFraudster){
+
+}
