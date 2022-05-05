d255644238b40153a9a046de4d6ec6bd0825fb0e
PREMINDA <premindabandara0777@gmail.com>
2022-05-06 04:11:25 +0530

    Fix NotificationRequest

--- clients/src/main/java/com/example/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/example/clients/notification/NotificationClient.java
@@ -1,7 +1,11 @@
 package com.example.clients.notification;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient("NOTIFICATION")
 public interface NotificationClient {
+    @PostMapping("api/v1/notification")
+    void sendNotification(NotificationRequest notificationRequest);
 }
--- clients/src/main/java/com/example/clients/notification/NotificationRequest.java
+++ clients/src/main/java/com/example/clients/notification/NotificationRequest.java
@@ -1,8 +1,5 @@
 package com.example.clients.notification;
 
-import lombok.ToString;
-
-@ToString
 public record NotificationRequest(Integer toCustomerId,
         String toCustomerEmail,
         String sender, String message
